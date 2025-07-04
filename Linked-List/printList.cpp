43c6beec188c8bb78122e46d8d694c7c83c1044b
kgxkaran <karangera025@gmail.com>
2025-07-04 14:09:56 +0530

    Create printList.cpp

---
+++ Linked-List/printList.cpp
@@ -0,0 +1,38 @@
+void PrintList(Node*head){
+    while(head != NULL){
+        cout << head->data << endl;
+        cout << head->next << endl;
+        head = head->next;
+    }
+}
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data;
+    Node*next;
+    Node(int x){
+        data = x;
+        next = NULL;
+    };
+};
+void PrintList(Node*head){
+    while(head != NULL){
+        cout << head->data << endl;
+        cout << head->next << endl;
+        head = head->next;
+    }
+}
+
+int main(){
+    Node*first = new Node(34);
+    Node*second = new Node(64);
+    Node*third = new Node(53);
+    first->next = second;
+    second->next = third;
+    PrintList(first);
+    
+}
