d7a0b8c8be0966adfa1f3a9309627080c45ba232
kgxkaran <karangera025@gmail.com>
2025-07-05 16:38:26 +0530

    Create Factorial.cpp

---
+++ Basic-Maths/Factorial.cpp
@@ -0,0 +1,17 @@
+//iterative approach
+
+int factorial(int n) {
+    int fact = 1;
+    for (int i = 1; i <= n; i++) {
+        fact = fact * i;
+    }
+    return fact;
+}
+
+
+//recursive
+
+int factorial(int n) {
+    if (n == 0) return 1;
+    return n * factorial(n - 1);
+}
