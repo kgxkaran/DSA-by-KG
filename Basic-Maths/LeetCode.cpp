74e32e5f0454a1d017652ce2ee9ba5b2b7dc635f
kgxkaran <karangera025@gmail.com>
2025-07-05 17:07:01 +0530

    Create LeetCode.cpp

---
+++ Basic-Maths/LeetCode.cpp
@@ -0,0 +1,12 @@
+//Trailin zeroes :- count number of 5s
+
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int ans = 0;
+        for(int i = 5; i <= n; i *= 5) {
+            ans += n / i;
+        }
+        return ans;
+    }
+};
