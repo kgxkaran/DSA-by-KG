39b706daf1a2459c194bf17e2da3c778299dbf05
kgxkaran <karangera025@gmail.com>
2025-07-10 17:06:25 +0530

    Update LeetCodeQ234.cpp

--- Linked-List/Single-Linked-List/LeetCodeQ234.cpp
+++ Linked-List/Single-Linked-List/LeetCodeQ234.cpp
@@ -29,3 +29,40 @@ public:
 Approch 2 :- Using fast-slow pointers
 
 
+class Solution {
+public:
+    ListNode*reverse(ListNode* head){
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+
+        while(curr != NULL){
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    bool isPalindrome(ListNode* head) {
+        ListNode*fast = head;
+        ListNode*slow = head;
+        
+
+        while(fast != NULL && fast->next != NULL){
+            
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* secondHalf = reverse(slow);
+        ListNode* firstHalf = head;
+
+        while(secondHalf){
+            if(secondHalf->val != firstHalf->val) return false;
+            secondHalf = secondHalf->next;
+            firstHalf = firstHalf->next;
+        }
+        return true;
+        
+        
+
+
